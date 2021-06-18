5a86182d70bface4782c7fe209f2c81f7a281b01
linkoid <36754150+linkoid@users.noreply.github.com>
2021-06-18 14:37:38 -0400

    Fixes and Example .blend file

--- Vaporwave/__init__.py
+++ Vaporwave/__init__.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Vaporwave",
-    "author": "@linkoid",
+    "author": "linkoid",
     "version": ("alpha", 2021, 6, 18),
     "blender": (2, 80, 0),
     "location" : "View 3D > Sidebar > Vaporwave",
@@ -8,25 +8,27 @@ bl_info = {
     "warning": "",
     "wiki_url": "https://github.com/linkoid/blender-vaporwave/blob/main/README.md",
     "tracker_url": "https://github.com/linkoid/blender-vaporwave",
-    "category": "Modeling"
+    "category": "Mesh"
 }
+
+import importlib
 
 import_modules = ["space_view3d_vaporwave"]
 imported_modules = dict()
 
 if "bpy" in locals():
-    import imp
-
     for module in imported_modules.values():
-        imp.reload(module)
+        importlib.reload(module)
 
 else:
-    import importlib
     for module_name in import_modules:
-        module = imp.import_modules(f".{module_name}")
-        locals()[module_name] = module
+        module = importlib.import_module(f"{__package__}.{module_name}")
+        #locals()[module_name] = module
         imported_modules[module_name] = module
 
+
+
+import bpy
 
 class AddonPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__
